4dfcfa2ffa20457330300621d8761582aef4c726
Nicholas Junge <nicho.junge@gmail.com>
2026-05-26 14:26:16 +0200

    feature: Create sitemap, Google Search token secret, robots.txt
    The token is stored as a GitHub secret and injected into the build.

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,34 @@
+import { getNotes, normalizeTag } from '$lib/utils/notes';
+import { getPhotography } from '$lib/utils/photography';
+
+const SITE = 'https://nicholasjunge.com';
+
+export const prerender = true;
+
+export async function GET() {
+	const [notes, photography] = await Promise.all([getNotes(), getPhotography()]);
+
+	const tags = new Set<string>();
+	for (const n of notes) n.metadata.tags?.forEach((t) => tags.add(normalizeTag(t)));
+	for (const p of photography) p.metadata.tags?.forEach((t) => tags.add(normalizeTag(t)));
+
+	const urls = [
+		'/',
+		'/about',
+		'/notes',
+		'/photography',
+		...notes.map((n) => `/notes/${n.slug}`),
+		...photography.map((p) => `/photography/${p.slug}`),
+		...[...tags].map((t) => `/tags/${t}`)
+	];
+
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((u) => `  <url><loc>${SITE}${u}</loc></url>`).join('\n')}
+</urlset>
+`;
+
+	return new Response(xml, {
+		headers: { 'Content-Type': 'application/xml' }
+	});
+}
